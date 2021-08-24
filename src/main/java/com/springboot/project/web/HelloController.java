7db2c39219917a07ae6c8a7aa7b5c1e82bc0385c
BaekYujin <byujin66@gmail.com>
2021-08-24 21:12:00 +0900

    20210824 - 롬북 이용한 소스 추가 dto

--- src/main/java/com/springboot/project/web/HelloController.java
+++ src/main/java/com/springboot/project/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.springboot.project.web;
 
+import com.springboot.project.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String Hello(){
         return "Hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto hellodto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
